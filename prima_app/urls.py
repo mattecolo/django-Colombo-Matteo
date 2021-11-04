21f529da357d69bdf85469675036f87d3b8f14ef
mattecolo <teocolombo03@gmail.com>
2021-11-04 10:58:50 +0100

    Livello4

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns=[
     path('welcome', welcome, name='welcome'),
     path("lista",lista,name="lista"),
     path("chi_siamo",chi_siamo,name="chi_siamo"),
+    path("variabili",variabili,name="variabili"),
 ]
