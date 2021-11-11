6e05c980b60f9d443d60de206707f301ceb0c01f
mattecolo <teocolombo03@gmail.com>
2021-11-11 10:15:47 +0100

    Livello7

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import es_if
+from .views import es_if,if_else_elif
 
 app_name="seconda_app"
 urlpatterns = [
     path("es_if",es_if,name="es_if"),
+    path("if_else_elif",if_else_elif,name="if_else_elif"),
 ]
