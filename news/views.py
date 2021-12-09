50d483b6ece3f1d9b595b036dfafd8ace410a759
mattecolo <teocolombo03@gmail.com>
2021-12-09 12:00:58 +0100

    Livello13

---
+++ news/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Articolo,Giornalista
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+def home(request):
+    articoli=Articolo.objects.all()
+    giornalisti=Giornalista.objects.all()
+    context={"articoli": articoli, "giornalisti": giornalisti}
+    print(context)
+    return render(request, "homepage.html", context)
+
+def ArticoloDetailView(request,pk):
+    articolo=get_object_or_404(Articolo,pk=pk)
+    context={"articolo":articolo}
+    return render(request, "articolo_details.html", context)
+
+def GiornalistaDetailView(request,pk):
+    gionalista=get_object_or_404(Giornalista,pk=pk)
+    context={"giornalista":giornalista}
+    return render(request, "giornalista_details.html", context)
+
+class ArticoloDetailViewCB(DetailView):
+    model=Articolo
+    template_name="articolo_detail.html"
+
+class ArticoloListView(ListView):
+    model=Articolo
+    template_name="lista_articoli.html"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context["articoli"]=Articolo.objects.all()
+        return context
+
+class GiornalistaDetailViewCB(DetailView):
+    model=Giornalista
+    template_name="giornalista_detail.html"
+
+class GiornalistaListView(ListView):
+    model=Giornalista
+    template_name="lista_giornalisti.html"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context["giornalisti"]=Giornalista.objects.all()
+        return context
